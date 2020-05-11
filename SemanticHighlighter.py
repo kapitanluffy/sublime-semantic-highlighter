5a0830be653c3fef6efdb945ae4e5bd3a194b2e4
kapitanluffy <kapitanluffy@gmail.com>
2020-05-12 00:20:43 +0800

    Unbind on change events

--- SemanticHighlighter.py
+++ SemanticHighlighter.py
@@ -7,27 +7,30 @@ import sublime_plugin
 from .src import *
 
 def plugin_loaded():
-    print("Semantic Highlighter loaded")
-
     settings = sublime.load_settings('SemanticHighlighter.sublime-settings')
 
     settings.clear_on_change('scopes')
-    settings.add_on_change('scopes', plugin_loaded)
-
     settings.clear_on_change('style')
+
+    settings.add_on_change('scopes', plugin_loaded)
     settings.add_on_change('style', plugin_loaded)
 
     state['style'] = styles[settings.get('style', 'underline')]
     state['syntax_scopes'] = settings.get('scopes', {})
 
+    print("Semantic Highlighter loaded")
+
+def plugin_unloaded():
+    settings = sublime.load_settings('SemanticHighlighter.sublime-settings')
+    settings.clear_on_change('scopes')
+    settings.clear_on_change('style')
+
 class SemanticHighlighterDebugCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("debug")
         analyzer = ScopeAnalyzer();
         selection = self.view.sel()
         analyzer.analyze(self.view, selection[0])
 
 class SemanticHighlighterMarkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("keyboard")
         self.view.run_command('semantic_highlighter_highlight', { "locked": True })
